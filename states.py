59873f29271bc8375d8b3477dcb2772402f05e9f
Dranik1 <125028857+Dranik1@users.noreply.github.com>
2024-11-11 08:42:17 +0200

    h

--- states.py
+++ states.py
@@ -3,4 +3,16 @@ with open('states.json') as f:
     data=json.load(f)
 
 for state in data['states']:
-    print(state)
+    print(state['name'], state['abbriviation'])
+
+for state in data['states']:
+    del state['area_codes']
+
+with open('new_states', 'w') as f:
+    json.dump(data, f, indent=4)
+
+for a in data['states']:
+    if a['name'][0]=="A":
+        print(a['name'])
+    else:
+        pass 
